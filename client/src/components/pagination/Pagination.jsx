b3d2430041f934664ebd6c8cef271452f42493da
JErickDev <jerickgm89@gmail.com>
2024-04-16 18:00:56 -0500

    Add estilos en general
    Se agrego estilos a paginador

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -8,10 +8,10 @@ export const Pagination = ({prevPage, nextPage, page}) => {
     <div className="mx-auto">
         <div className="grid grid-cols-3 place-items-center">
         <button className='btn btn-paginator' onClick={prevPage}>Prev</button>
-        <div className="grid grid-cols-3 gap-2">
-            <button className='btn btn-disabled'>{prevPageIndicator}</button>
+        <div className="paginationResponsive">
+            <button className='btn btn-disabled prevIndicator'>{prevPageIndicator}</button>
             <button className='btn btn-primary'>{page + 1}</button>
-            <button className='btn btn-disabled'>{nextPageIndicator}</button>
+            <button className='btn btn-disabled nextIndicator'>{nextPageIndicator}</button>
         </div>
         <button className='btn btn-paginator' onClick={nextPage}>Next</button>
       </div>
